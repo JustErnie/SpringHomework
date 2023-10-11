a49982bd1cea176ed3ca6d775a85a6b8f3880a39
Ernest <Letkeman1996@gmail.com>
2023-10-11 19:37:14 +0700

    Revert "Yes"
    This reverts commit 54a9150519ff496735df9d78feb2da7f2802604b.

--- src/main/java/Competition/ResultProcessorConfig.java
+++
@@ -1,9 +0,0 @@
-package Competition;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("Competition")
-public class ResultProcessorConfig {
-}
